1ef870d2dd9a1b8f40727d3c5524f2c9deb49cde
Gustavo Nunes Martins <martins.yo@gmail.com>
2021-02-09 16:33:29 +0100

    Motor PWM works

--- main/blink.c
+++ main/blink.c
@@ -11,6 +11,7 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include <driver/ledc.h>
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
@@ -18,14 +19,53 @@
 #define MOTOR 2
 #define LED 4
 
+
+
+// PWM
+
+void pwmtask(void* ignore){
+    ledc_timer_config_t timer_conf;
+    timer_conf.freq_hz=50;
+    timer_conf.duty_resolution=LEDC_TIMER_10_BIT;
+    timer_conf.timer_num=LEDC_TIMER_0;
+    timer_conf.speed_mode=LEDC_HIGH_SPEED_MODE;
+
+    ledc_timer_config(&timer_conf);
+
+    int duty=500;// 20/1024*62=1,25 ms
+
+    ledc_channel_config_t ledc_conf;
+    ledc_conf.channel=LEDC_CHANNEL_0;
+    ledc_conf.duty=duty; 
+    ledc_conf.gpio_num=MOTOR;
+    ledc_conf.intr_type=LEDC_INTR_DISABLE;
+    ledc_conf.speed_mode=LEDC_HIGH_SPEED_MODE;
+    ledc_conf.timer_sel=LEDC_TIMER_0;
+    ledc_channel_config(&ledc_conf);
+
+    
+    int step=100;
+    while(1){
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty);
+	ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+    
+    duty+=step;
+    if ( (duty <=0) || (duty >= 900)) {
+        step=-step;
+    }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        printf("PWM here: %d\n",duty);
+    }
+}
+
+//END PWM
+
 typedef struct {
     int led;
     int ticks_on;
     int ticks_off;
     bool reverted;
 } LEDParams;
-
-
 
 void task1(void* params){
 
@@ -56,12 +96,13 @@ void app_main()
        Technical Reference for a list of pads and their default
        functions.)
     */
-    LEDParams l1={        .led=MOTOR,    .ticks_on=2000,    .ticks_off=2000,.reverted=true};
-    LEDParams l2={        .led=LED,    .ticks_on=500,    .ticks_off=500,.reverted=false};
+    //LEDParams l1={        .led=MOTOR,    .ticks_on=2000,    .ticks_off=2000,.reverted=true};
+    LEDParams l2={        .led=LED,    .ticks_on=2000,    .ticks_off=2000,.reverted=false};
     printf("portTICK_PERIOD_MS: %d\n",portTICK_PERIOD_MS);
  
-   xTaskCreate(task1,"MOTOR",1024*2,&l1,5,NULL);
-   xTaskCreate(task1,"LED",1024*2,&l2,5,NULL);
+   //xTaskCreate(task1,"MOTOR",1024*2,&l1,5,NULL);
+   //xTaskCreatePinnedToCore(pwmtask,"MOTOR",1024*5,NULL,5,NULL,0);
+   xTaskCreatePinnedToCore(task1,"LED",1024*5,&l2,5,NULL,0);
     
    while(true){
        vTaskDelay(1000/portTICK_PERIOD_MS);
